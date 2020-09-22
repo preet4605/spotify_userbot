3ac59cb27a01036c575f115e6556cdfc01480427
preet4605 <51086787+preet4605@users.noreply.github.com>
2020-09-23 00:09:13 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ API_HASH = "860d356de4da4e6fcfa0fd3cf2abdf46"
 SESSION_KEY = "1BVtsOK8Bu0ZkRbylM9eq2N5nQbeYDysOygCPOQN2nxKlwPPPa6N5L0prID1pUM-7ZKrUuvh30_0iowgslwNPXXlqgyoU79-q1u47OTbvGxus3Iokci1-E_kaNUp1fjCgL1Yf9hHUFMPzqgY3ENgyYAqYfnTcsoTQGEEEoD7ALf4Hnyoyl8vY_rpTTAbpcWq0zh76Uc28NTxc_rRGlC77dRtElgmWRxb_h0q4NqtdTwc5SNOJv5EL1yi5CFGtHeDeMQDsqjX7R5eKbQLBlVuxArY59Ce_cV0lXLHyJljYE8WzWsW1xEVPip0LisMdSUME14McD4LWIiadymsuDWYT_LVQFZWUam8="
 INITIAL_TOKEN = "AQBfuqeM3pjTAH7PilHequhZnZoCCNcijgUsUZiw7tMLwU9VAhbeBoa6MACh557SWPYykIxW3R3udUiay8W0G1OX_qhmwOXiAvEMFd614GaOjsp3wYIVxWQMdy3GM3N_gMN2J0s5QHYFwsQYZif3tWbH_rnWM-hxHLyA98PwU_J1m-vAKZbPYcgm3tf7QbGl0n8xRHUUnJqtVN2jJsb2MqxoxS1Y6YLS9t0QRIC3SU1gzs_GnFcXvUo9UhnT-0hnNFzpcw-4fTTC4AkjI9cmdLtosN3D1u514a8D70qyhM7tVatY3PUZqhTk2IhUQysvg9o_GYewyAec1ynpSNRZnYXiLvoxaoOF2ylL5eGhk0lX3DDD-mRGi7w7LZ_IhrghjV1R-4dskd3Tq-_mQezE3grYSjZdnLd3ekdovC_UF2lioAMd1O1JJAyf"
 INITIAL_BIO = "Thank you."
-SCREENSHOT_LAYER_ACCESS_KEY = ""
-BOT_TOKEN = ''
+SCREENSHOT_LAYER_ACCESS_KEY = "none"
+BOT_TOKEN = '+919569037441'
 BOTLOG = True 
 LOG = -1001270871080
 # the escaping is necessary since we are testing against a regex pattern with it.
@@ -23,7 +23,7 @@ BIOS = [KEY + ' Vibing ; {interpret} - {title} {progress}/{duration}',
         KEY + ' Vibing : {title}',
         KEY + ' : {title}']
 
-OPEN_WEATHER_MAP_APPID = ' '
+OPEN_WEATHER_MAP_APPID = 'none'
 # Mind that some characters (e.g. emojis) count more in telegram more characters then in python. If you receive an
 # AboutTooLongError and get redirected here, you need to increase the offset. Check the special characters you either
 # have put in the KEY or in one of the BIOS with an official Telegram App and see how many characters they actually
